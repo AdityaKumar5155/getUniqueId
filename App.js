26840eab2133391296ff740e6d800f3c14d0262e
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-08-31 22:51:08 +0530

    feat : Added functionality to get Unique device id

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, ActivityIndicator } from 'react-native';
+import * as Application from 'expo-application';
+import * as SecureStore from 'expo-secure-store';
 
 export default function App() {
+  const [uniqueId, setUniqueId] = useState(null);
+
+  useEffect(() => {
+    const getOrCreateStableId = async () => {
+      try {
+        // Try to retrieve the unique ID from SecureStore
+        // await SecureStore.deleteItemAsync('uniqueDeviceId');
+        let storedUniqueId = await SecureStore.getItemAsync('uniqueDeviceId');
+        if (!storedUniqueId) {
+          // If no ID is found, create one using Application.androidId or Application.iosIdForVendor
+          storedUniqueId = Application.getAndroidId() || Application.getIosIdForVendorAsync()
+          console.log(storedUniqueId)
+          // Store the generated ID for future use
+          await SecureStore.setItemAsync('uniqueDeviceId', storedUniqueId);
+        }
+        setUniqueId(storedUniqueId);
+      } catch (error) {
+        console.error("Error generating or retrieving the device ID:", error);
+      }
+    };
+
+    getOrCreateStableId();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {uniqueId ? (
+        <Text>Your Unique Device ID: {uniqueId}</Text>
+      ) : (
+        <ActivityIndicator size="large" color="#0000ff" />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
